05a87384451c7d606c9771808934ba84b01a3c00
Juan Cruz <jc23729@gmail.com>
2021-01-05 21:09:23 -0500

    post.html and how to use jinja,and pull from our route function we created

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 
 app= Flask(__name__)
-
+#we created a list of dictionaries to create a dummy database
 all_posts= [
     {
         'title':'Post 1',
@@ -12,17 +12,22 @@ all_posts= [
         'content': 'This is the content of post 2. LAlalalala'
         }
     ]
+#main template 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+##we crearted a route and now that we have posts= all_posts(which is linked to our database)
+# we will now have access to variable posts that we can use in our html
 @app.route('/posts')
 def posts():
-    return render_template('posts.html')
+    return render_template('posts.html', posts = all_posts)
 
 @app.route('/home/<int:id>')
 def hello(id):
     return "Hello, " + str(id)
 
+#route that only gets get requests 
 @app.route('/onlyget', methods=['Get'])
 def get_reg():
     return 'You can only get this webpage. '
