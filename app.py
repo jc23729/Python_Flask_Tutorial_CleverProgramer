8dc1f86f3cff1a26d27c098c160ba78137eaf66e
Juan Cruz <jc23729@gmail.com>
2021-01-05 20:39:50 -0500

    working with templates

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app= Flask(__name__)
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 @app.route('/home/<int:id>')
 def hello(id):
     return "Hello, " + str(id)
 
-@app.route('/onlyget')
+@app.route('/onlyget', methods=['Get'])
 def get_reg():
     return 'You can only get this webpage. '
 
