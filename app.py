7977f8441e395907787d264ebdcdb11ace19cf11
Juan Cruz <jc23729@gmail.com>
2021-01-05 20:29:13 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,13 @@ from flask import Flask
 
 app= Flask(__name__)
 
-@app.route('/')
-def hello():
-    return "Hello World"
+@app.route('/home/<int:id>')
+def hello(id):
+    return "Hello, " + str(id)
+
+@app.route('/onlyget')
+def get_reg():
+    return 'You can only get this webpage. '
 
 if __name__ == "__main__":
     app.run(debug=True)
