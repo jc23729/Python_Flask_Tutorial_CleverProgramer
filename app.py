51bfdeeca87d06000f9823edf22ad8fc7c56969a
Juan Cruz <jc23729@gmail.com>
2021-01-05 20:57:17 -0500

    base.html, fake database all_posts
    built base.html template, dummy code or fake database, all_posts

--- app.py
+++ app.py
@@ -1,9 +1,23 @@
 from flask import Flask, render_template
 
 app= Flask(__name__)
+
+all_posts= [
+    {
+        'title':'Post 1',
+        'content': 'This is the content of post 1. LAlalalala'
+        },
+        {
+        'title':'Post 2',
+        'content': 'This is the content of post 2. LAlalalala'
+        }
+    ]
 @app.route('/')
 def index():
     return render_template('index.html')
+@app.route('/posts')
+def posts():
+    return render_template('posts.html')
 
 @app.route('/home/<int:id>')
 def hello(id):
